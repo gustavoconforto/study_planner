03995365797e38b09c2255a87ccb9553fcb6861f
Gustavo Conforto <gustavoconforto@gmail.com>
2026-07-29 14:04:59 -0300

    feat: add `disponibilidade`page

---
+++ app/disponibilidade/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../layout/PageHeader";
+
+export default function DisponibilidadePage() {
+  return (
+    <div>
+      <Header
+        title="Disponibilidade"
+        subtitle="Insira sua disponibilidade de estudos para que possamos ajudar na montagem ho seu horário"
+      />
+    </div>
+  );
+}

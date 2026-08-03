ed7a485026b9a2bd5479b82392772a833cd8478a
Gustavo Conforto <gustavoconforto@gmail.com>
2026-08-03 08:19:59 -0300

    chore: create schema for `task`

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,11 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  varchar,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
 export const agendamentoTable = pgTable("agendamentos", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,4 +13,52 @@ export const agendamentoTable = pgTable("agendamentos", {
   weekday: integer().notNull(),
   start: varchar({ length: 10 }).notNull(),
   finish: varchar({ length: 10 }).notNull(),
+});
+
+export const TASK_TYPE = [
+  "DEVER",
+  "PS",
+  "PB",
+  "REVISÃO",
+  "LEITURA",
+  "REDAÇÃO",
+  "SIMULADO",
+  "OUTROS",
+] as const;
+export const TASK_DIFICULTY = ["FÁCIL", "MÉDIO", "DIFÍCIL"] as const;
+export const TASK_STATUS = ["AGENDADO", "CADASTRADO"] as const;
+export const TASK_SUBJECT = [
+  "LÍNGUA PORTUGUESA",
+  "ARTES",
+  "MATEMÁTICA",
+  "BIOLOGIA",
+  "QUÍMICA",
+  "FÍSICA",
+  "HISTÓRIA",
+  "GEOGRAFIA",
+  "FILOSOFIA",
+  "SOCIOLOGIA",
+  "LÍNGUA ESTRANGEIRA",
+  "PROJETOS",
+  "OUTROS",
+] as const;
+
+export const taskTypeEnum = pgEnum("task_type", TASK_TYPE);
+export const taskDificultyEnum = pgEnum("task_dificulty", TASK_DIFICULTY);
+export const taskStatusEnum = pgEnum("task_status", TASK_STATUS);
+export const tasksubjectEnum = pgEnum("task_subject", TASK_SUBJECT);
+
+export const tarefaTable = pgTable("tasks", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  type: taskTypeEnum("task_type").notNull(),
+  dificulty: taskDificultyEnum("task_dificulty").notNull(),
+  status: taskStatusEnum("task_status").notNull(),
+  subject: tasksubjectEnum("task_subject").notNull(),
+  student_email: varchar({ length: 255 }).notNull(),
+  title: varchar({ length: 255 }).notNull(),
+  description: text().notNull(),
+  estimated_minutes: integer().notNull(),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updated_at").$onUpdate(() => new Date()),
+  completed_at: timestamp("completed_at"),
 });
